e9e49f68f481a6c24c5adc65e509257bb63973d8
wsolichin-sees <wsolichin@sees.ai>
2023-10-03 15:44:13 +0100

    Updating RTK handling RTCM

--- src/mavsdk/plugins/rtk/rtk_impl.cpp
+++ src/mavsdk/plugins/rtk/rtk_impl.cpp
@@ -29,44 +29,44 @@ void RtkImpl::disable() {}
 Rtk::Result RtkImpl::send_rtcm_data(Rtk::RtcmData rtcm_data)
 {
     constexpr size_t field_len = MAVLINK_MSG_GPS_RTCM_DATA_FIELD_DATA_LEN;
-
-    const size_t num_packets_required =
-        rtcm_data.data.size() / field_len + (rtcm_data.data.size() % field_len == 0 ? 0 : 1);
-
-    // The maximum is 4 times the 180 bytes because we only have two bits to
-    // denote the fragment ID.
-    if (num_packets_required > 4) {
-        return Rtk::Result::TooLong;
-    }
-
-    // Copy length before we change it.
-    size_t bytes_to_send = rtcm_data.data.size();
-
-    // The mavlink helpers memcpy, so we need to make sure we're not
-    // copying from where we shouldn't.
-    rtcm_data.data.resize(num_packets_required * field_len);
-
-    for (size_t i = 0; i < num_packets_required; ++i) {
-        const uint8_t flags =
-            (num_packets_required > 1 ? 0x1 : 0x0) | ((i & 0x3) << 1) | ((_sequence & 0x1F) << 3);
-
+    
+    mavlink_gps_rtcm_data_t mavlink_rtcm_data;
+    
+    memset(&mavlink_rtcm_data, 0, sizeof(mavlink_gps_rtcm_data_t));
+    
+    if (rtcm_data.data.size() < field_len) {
+        mavlink_rtcm_data.len = rtcm_data.data.size();
+        mavlink_rtcm_data.flags = (_sequence & 0x1F) << 3;
+        memcpy(&mavlink_rtcm_data.data, rtcm_data.data.data(), rtcm_data.data.size());
         mavlink_message_t message;
         mavlink_msg_gps_rtcm_data_pack(
-            _parent->get_own_system_id(),
-            _parent->get_own_component_id(),
-            &message,
-            flags,
-            static_cast<uint8_t>(std::min(field_len, bytes_to_send)),
-            reinterpret_cast<const uint8_t*>(rtcm_data.data.c_str() + (i * field_len)));
-
-        if (!_parent->send_message(message)) {
-            ++_sequence;
-            return Rtk::Result::ConnectionError;
+            _parent->get_own_system_id(), 
+            _parent->get_own_component_id(), 
+            &message, 
+            mavlink_rtcm_data.flags, 
+            mavlink_rtcm_data.len, 
+            mavlink_rtcm_data.data);
+    } else {
+        uint8_t fragment_id = 0;
+        int start = 0;
+        while (start < rtcm_data.data.size()) {
+            int length = std::min(rtcm_data.data.size() - start, field_len);
+            mavlink_rtcm_data.flags = 1;
+            mavlink_rtcm_data.flags |= fragment_id++ << 1;
+            mavlink_rtcm_data.flags |= (_sequence & 0x1F) << 3;
+            mavlink_rtcm_data.len = length;
+            memcpy(&mavlink_rtcm_data.data, rtcm_data.data.data() + start, length);
+            mavlink_message_t message;
+            mavlink_msg_gps_rtcm_data_pack(
+            _parent->get_own_system_id(), 
+            _parent->get_own_component_id(), 
+            &message, 
+            mavlink_rtcm_data.flags, 
+            mavlink_rtcm_data.len, 
+            mavlink_rtcm_data.data);
+            start += length;
         }
-
-        bytes_to_send -= field_len;
     }
-
     ++_sequence;
     return Rtk::Result::Success;
 }
